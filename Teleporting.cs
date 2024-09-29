455ce8f9030fa95db38dc3c9ea672e2771692536
Roberts Cēsnieks <roberts.cesnieks@gmail.com>
2024-09-29 19:21:53 +0300

    Update Teleporting.cs

--- Teleporting.cs
+++ Teleporting.cs
@@ -1,16 +1,1 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class Teleporting : MonoBehaviour
-{
-    public transform teleportTarget;
-    public GameObject thePlayer;
-
-
-    void OnTriggerEnter
-
-    {
-        thePlayer.transform.position = teleportTarget.transform.position;
-    }
-}
