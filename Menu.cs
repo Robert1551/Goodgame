3a390fd7506b1cee91fe358e45d467ebfaa389c2
Roberts Cēsnieks <roberts.cesnieks@gmail.com>
2024-10-06 17:20:58 +0300

    Update Menu.cs

--- Menu.cs
+++ Menu.cs
@@ -1,14 +1,17 @@
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-
-    public void EnterGame() 
+    public static void EnterGame() 
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);    
     }
 
-
-
+    [MenuItem("Helpers/Restart Scene #E")]
+    public static void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 }
